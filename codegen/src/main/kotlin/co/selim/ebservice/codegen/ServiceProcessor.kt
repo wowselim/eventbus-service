967b26ad16f797552dcc739061f05fea229288c1
Selim Dincer <wowselim@live.de>
2021-03-12 09:13:50 +0100

    draft v2 with interface implementations

--- codegen/src/main/kotlin/co/selim/ebservice/codegen/ServiceProcessor.kt
+++ codegen/src/main/kotlin/co/selim/ebservice/codegen/ServiceProcessor.kt
@@ -2,13 +2,18 @@ package co.selim.ebservice.codegen
 
 import co.selim.ebservice.annotation.EventBusService
 import com.squareup.kotlinpoet.ClassName
-import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
+import com.squareup.kotlinpoet.TypeName
+import com.squareup.kotlinpoet.metadata.*
+import kotlinx.metadata.KmClassifier
 import javax.annotation.processing.*
 import javax.lang.model.SourceVersion
+import javax.lang.model.element.Element
 import javax.lang.model.element.TypeElement
-import javax.lang.model.type.MirroredTypeException
 import javax.lang.model.util.ElementFilter
+import javax.tools.Diagnostic
 
+@KotlinPoetMetadataPreview
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 @SupportedOptions("kapt.kotlin.generated")
 @SupportedAnnotationTypes("co.selim.ebservice.annotation.EventBusService")
@@ -18,59 +23,119 @@ class ServiceProcessor : AbstractProcessor() {
     if (elements.isEmpty()) return false
 
     val typeElements = ElementFilter.typesIn(elements)
-    val methodElements = ElementFilter.methodsIn(elements)
 
-    (typeElements + methodElements).forEach { element ->
-      element.getAnnotation(EventBusService::class.java)?.let { service ->
-        val consumedType = service.extractConsumedType()
-        val producedType = service.extractProducedType()
-
-        val sourceFile = generateServiceExtensions(
-          service.topic,
-          consumedType,
-          producedType,
-          service.propertyName,
-          service.functionName
-        )
-        sourceFile.writeTo(processingEnv.filer)
+    val services = typeElements.map { typeElement ->
+      val kmClass = typeElement.toImmutableKmClass()
+      if (!kmClass.isInterface) {
+        logError("Only interfaces are supported by ebservice", typeElement)
+        error("Only interfaces are supported by ebservice")
       }
+
+      val functions = kmClass.functions
+        .asSequence()
+        .filter { kmFunction ->
+          val classifier = kmFunction.returnType.classifier
+          classifier is KmClassifier.Class || classifier is KmClassifier.TypeAlias
+        }
+        .extractFunctions()
+        .toSet()
+
+      Service(kmClass.name.toClassName(), functions)
+    }
+
+    services.forEach { service ->
+      val fileSpecBuilder = generateFile(service.name)
+      fileSpecBuilder.addType(
+        generateServiceImpl(service)
+          .apply { generateFunctions(fileSpecBuilder, this, service.functions) }
+          .build()
+      )
+      generateRequestProperties(service.name, service.functions).forEach(fileSpecBuilder::addProperty)
+      fileSpecBuilder.build()
+        .writeTo(processingEnv.filer)
     }
 
     return true
   }
 
-  private fun EventBusService.extractConsumedType(): ClassName {
-    return extractClass { ClassName.bestGuess(consumes.java.name) }
-  }
+  private fun Sequence<ImmutableKmFunction>.extractFunctions(): Sequence<Function> {
+    return map { kmFunction ->
+      if (!kmFunction.isSuspend) {
+        logError("Function ${kmFunction.name} must be suspending")
+        error("Function ${kmFunction.name} must be suspending")
+      }
 
-  private fun EventBusService.extractProducedType(): ClassName {
-    return extractClass { ClassName.bestGuess(produces.java.name) }
-  }
+      val typeParameters = kmFunction.returnType.getTypeParameters()
+      val returnType = kmFunction.returnType.classifier
+        .toClassName()
+        .safelyParameterizedBy(typeParameters)
 
-  private fun extractClass(block: () -> ClassName): ClassName {
-    return try {
-      block()
-    } catch (e: MirroredTypeException) {
-      val typeElement = processingEnv.typeUtils.asElement(e.typeMirror) as TypeElement
-      val qualifiedName = typeElement.qualifiedName.toString()
-      ClassName.bestGuess(qualifiedName)
+      val parameters = kmFunction.valueParameters
+        .asSequence()
+        .onEach { parameter ->
+          if (parameter.varargElementType != null) {
+            logError("Vararg parameters are not supported by ebservice")
+            error("Vararg parameters are not supported by ebservice")
+          }
+        }
+        .filter { parameter ->
+          val classifier = parameter.type!!.classifier
+          classifier is KmClassifier.Class || classifier is KmClassifier.TypeAlias
+        }
+        .toFunctionParameters()
+        .toSet()
+
+      Function(kmFunction.name, returnType, parameters)
     }
+  }
+
+  private fun Sequence<ImmutableKmValueParameter>.toFunctionParameters(): Sequence<Parameter> {
+    return map { kmValueParameter ->
+      val classifier = kmValueParameter.type!!.classifier
+      val typeParametersOfParameter = kmValueParameter.type!!.getTypeParameters()
+      val typeOfParameter = classifier.toClassName().safelyParameterizedBy(typeParametersOfParameter)
+      Parameter(kmValueParameter.name, typeOfParameter)
+    }
+  }
+
+  private fun logError(msg: String, element: Element? = null) {
+    processingEnv.messager.printMessage(Diagnostic.Kind.ERROR, msg, element)
+  }
+
+  private fun ImmutableKmType.getTypeParameters(): List<TypeName> {
+    return arguments.mapNotNull { typeProjection ->
+      val params = typeProjection.type?.getTypeParameters()
+
+      when (val classifier = typeProjection.type?.classifier) {
+        is KmClassifier.Class -> classifier.toClassName().safelyParameterizedBy(params)
+        is KmClassifier.TypeParameter,
+        is KmClassifier.TypeAlias,
+        null -> null
+      }
+    }
+  }
+
+  private fun ClassName.safelyParameterizedBy(typeNames: List<TypeName>?): TypeName {
+    return if (typeNames.isNullOrEmpty()) {
+      this
+    } else {
+      this.parameterizedBy(typeNames)
+    }
+  }
+
+  private fun KmClassifier.toClassName(): ClassName {
+    return when (this) {
+      is KmClassifier.Class -> name.toClassName()
+      is KmClassifier.TypeAlias -> name.toClassName()
+      is KmClassifier.TypeParameter -> error("Type parameters are not supported")
+    }
+  }
+
+  private fun String.toClassName(): ClassName {
+    return ClassName(substringBeforeLast('/').replace('/', '.'), substringAfterLast('/'))
   }
 
   override fun getSupportedAnnotationTypes(): Set<String> {
     return setOf(EventBusService::class.java.name)
-  }
-
-  private fun generateServiceExtensions(
-    topic: String,
-    consumedType: ClassName,
-    producedType: ClassName,
-    propertyName: String,
-    functionName: String
-  ): FileSpec {
-    return generateFile(topic)
-      .addProperty(generateRequestsProperty(consumedType, producedType, propertyName))
-      .addFunction(generateRequestFunction(consumedType, producedType, functionName))
-      .build()
   }
 }
